41f1f584d700a0367757433bdf59b551453d5eff
SG1515 <sunggu507@naver.com>
2022-08-03 11:11:06 +0900

    채팅 메세지 등록

--- src/main/java/com/ll/exam/chat/ChatMessageRepository.java
+++ src/main/java/com/ll/exam/chat/ChatMessageRepository.java
@@ -4,6 +4,7 @@ import com.ll.exam.chat.dto.ChatMessageDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ChatMessageRepository {
@@ -19,7 +20,7 @@ public class ChatMessageRepository {
 
     private static void makeTestData() {
         IntStream.rangeClosed(1, 10).forEach(roomId -> {
-            IntStream.rangeClosed(1, 10).forEach(id -> {
+            IntStream.rangeClosed(1, 2).forEach(id -> {
                 String body = "메세지 %d".formatted(id);
                 write(roomId, body);
             });
@@ -34,4 +35,12 @@ public class ChatMessageRepository {
 
         return id;
     }
+
+    public List<ChatMessageDto> findByRoomId(long roomId) {
+        return datum
+                .stream()
+                .filter(chatMessageDto -> chatMessageDto.getRoomId() == roomId)
+                .collect(Collectors.toList());
+    }
+
 }
